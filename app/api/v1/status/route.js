1afddc1963ea835fb54050f58a3a4d173f24aa7f
Lucas de Oliveira da SIlva <oliluc4@gmail.com>
2025-10-15 10:21:50 +0000

    Add database module with query function

--- app/api/v1/status/route.js
+++ app/api/v1/status/route.js
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
+import database from "../../../../infra/database.js";
 
 export async function GET() {
+  const result = await database.query("SELECT 1 + 1 AS sum");
+  console.log(result.rows);
   return NextResponse.json({ data: "São acima da média" }, { status: 200 });
 }
